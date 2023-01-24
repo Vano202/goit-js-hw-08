04b40a9925d0ea6db893115b8b215b90b40f92ae
Ivan Kravchenko <109036277+Vano202@users.noreply.github.com>
2023-01-24 19:14:00 +0200

    new

---
+++ src/js/01-gallery.js
@@ -0,0 +1,23 @@
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+const galleryImage = document.querySelector(".gallery");
+
+function addGallery(galleryItems) {
+  const markup = galleryItems.map(img =>
+`<a class="gallery__item" href="${img.original}">
+<img class="gallery__image" src="${img.preview}" alt="${img.description}" />
+</a>`).join('');
+  galleryImage.innerHTML = markup
+};
+addGallery(galleryItems);
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
+console.log(galleryItems);
---
+++ src/js/02-video.js
@@ -0,0 +1,22 @@
+var throttle = require('lodash.throttle');
+import Player from '@vimeo/player';
+
+const player = new Player("video-player", {
+    id: 19231868,
+    width: 640
+});
+console.log(player);
+
+var onPlay = function(data) {  
+  localStorage.setItem("videoplayer-current-time", JSON.stringify(data));
+};
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const savedSettings = localStorage.getItem("videoplayer-current-time");
+const parsedSettings = JSON.parse(savedSettings);
+console.log(parsedSettings);
+
+player.setCurrentTime(parsedSettings.seconds).then(function(seconds) {
+  console.log(seconds); 
+});
+
